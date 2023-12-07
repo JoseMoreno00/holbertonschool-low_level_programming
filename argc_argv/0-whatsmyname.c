7c46dce4187b32b5e2e02160ecda8530891ecb77
accgitjosem@gmail.com <JoseMoreno00>
2023-12-07 08:21:38 -0800

    Restore Backup

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Fuction that print file name.
+ * @argv: argv[0] = file name
+ * @argc: argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
